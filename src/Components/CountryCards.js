b49fe92eac31e46738f3b87877d210a080a93901
sbkzoomin <bharathsriram5@gmail.com>
2020-10-08 17:36:44 +0530

    1 file modified

--- src/Components/CountryCards.js
+++ src/Components/CountryCards.js
@@ -8,8 +8,9 @@ class CountryCards extends React.Component {
           <div className="row">
             <div className="col-md-3">
              <div className='card'>
-             <img src="https://www.w3schools.com/tags/img_girl.jpg"/>
+             <img src="https://www.w3schools.com/tags/img_girl.jpg" className='img-fluid'/>
               <div className='card-body'>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula diam commodo, egestas dui eu, tincidunt augue.</p>
               </div>
              </div> 
             </div>
