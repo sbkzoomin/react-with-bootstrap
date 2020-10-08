ede7ce0d3bd3eb3dd70cb061344372c91cfe85b7
sbkzoomin <bharathsriram5@gmail.com>
2020-10-08 05:30:31 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "mdbootstrap/css/bootstrap.css";
+import "mdbootstrap/css/mdb.css";
+import "./style.css";
+
 
 import React from "react";
 import ReactDOM from "react-dom";
