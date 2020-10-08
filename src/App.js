a46425b2ad301fee868d1767338f7c931734bafd
sbkzoomin <bharathsriram5@gmail.com>
2020-10-08 06:06:45 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CountryCards from './Components/CountryCards';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <nav className='navbar navbar-dark bg-primary'>
         <a href="#" className='navbar-brand'>React JS with Bootstrap</a>
       </nav>
+      <CountryCards></CountryCards>
     </div>
   );
 }
